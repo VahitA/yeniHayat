915b0356673d10822dd977d39beb1326ec0b72e5
vahita <vahitakpinar50@gmail.com>
2022-12-22 08:27:44 +0300

    v13

---
+++ src/calis/C04.java
@@ -0,0 +1,14 @@
+package calis;
+
+public class C04 {
+    public static void main(String[] args) {
+
+
+        int i=8;
+        System.out.println(i);
+
+        i=7;
+
+        System.out.println(i);
+    }
+}
