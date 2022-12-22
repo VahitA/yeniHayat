82ceb25ad36728383572fccbfbe6fc81094b742a
vahita <vahitakpinar50@gmail.com>
2022-12-22 13:33:40 +0300

    v17

--- src/calis/C04.java
+++ src/calis/C04.java
@@ -2,13 +2,6 @@ package calis;
 
 public class C04 {
     public static void main(String[] args) {
-
-
-        int i=8;
-        System.out.println(i);
-
-        i=7;
-
-        System.out.println(i);
+        System.out.println("eskiye dondu");
     }
 }
